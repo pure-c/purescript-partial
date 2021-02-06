58a3db23db423ba08b8f41a0544b239ecbaa7fd4
Felix Schlitter <felixschlitter@gmail.com>
2021-02-06 20:59:50 +1300

    Use purs_any_force_*

--- src/Partial.c
+++ src/Partial.c
@@ -1,6 +1,7 @@
 #include <purescript.h>
 
 PURS_FFI_FUNC_2(Partial_crashWith, _, msg) {
-	const purs_str_t *str = purs_any_get_string(msg);
+	const purs_str_t *str = purs_any_force_string(msg);
 	purs_assert(0, str->data);
+	PURS_RC_RELEASE(str);
 }
